f6d7c111d9b55708acd44a68b9dd310f31f9da07
shinnosuke.chuman@gmail.com <shinnosuke.chuman@gmail.com>
2018-01-13 10:39:01 +0800

    Initial Commit

---
+++ MCQ Quiz.Tests/MCQ_Quiz_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MCQ_Quiz_Tests.swift
+//  MCQ Quiz.Tests
+//
+//  Created by Shinnosuke Chuman on 2018/01/13.
+//  Copyright © 2018年 Shinnosuke Chuman. All rights reserved.
+//
+
+import XCTest
+@testable import MCQ_Quiz_
+
+class MCQ_Quiz_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
